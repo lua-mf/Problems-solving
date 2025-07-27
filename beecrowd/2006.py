dc1f3417d182a939792332fbd7ac181f77fa2120
luana <llmaf@cesar.school>
2025-07-27 11:32:40 -0300

    explicação do uso da função list

--- beecrowd/2006.py
+++ beecrowd/2006.py
@@ -1,3 +1,5 @@
 t = int(input())
+# map retorna um objeto do tipo map, não uma lista
+# list tranforma o objeto do tipo map em uma lista
 answers = list(map(int,input().split()))
 print(answers.count(t))
