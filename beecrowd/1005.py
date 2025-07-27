e3af6a23aab440e8af230cd0d06995142b88c68a
luana <llmaf@cesar.school>
2025-07-27 11:03:18 -0300

    solução 1005

---
+++ beecrowd/1005.py
@@ -0,0 +1,8 @@
+a = float(input())
+b = float(input())
+media = (a * 3.5 + b * 7.5) / 11
+# :.5f 
+# : inicia a formatação
+# .5 define 5 casas decimais
+# f indica que é número com ponto flutuante (float)
+print(f"MEDIA = {media:.5f}") 
