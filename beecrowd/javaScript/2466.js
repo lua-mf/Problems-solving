1033746e937d5bdba6e7118d19acdb239cbbba9a
lua-mf <llmaf@cesar.school>
2026-09-09 20:20:23 -0300

    add implementation to determine ball colors based on previous row configuration

--- beecrowd/javaScript/2466.js
+++ beecrowd/javaScript/2466.js
@@ -1,7 +1,17 @@
-const n = 5 // número de bolas da primeira fileira
-const primeiraFileira = [1, -1, 1, 1, -1] // preta = “1” ou branca = “-1”
+var input = require('fs').readFileSync('stdin', 'utf8');
+var lines = input.split('\n');
 
+let fileira = lines[1]
+let fileiraAuxiliar = []
 //  Ao se colocar uma bola na nova fileira, ela ficará encostada em duas bolas da fileira anterior e sua cor será:
 
 // Preta, se estiver encostada em duas bolas de mesma cor;
 // Branca, se estiver encostada em duas bolas de cores diferentes.
+for (let i = 0; i < fileira.length - 1; i++) {
+    if (fileira[i] === fileira[i + 1]) {
+        fileiraAuxiliar.push(1)
+    } else {
+        fileiraAuxiliar.push(-1)
+    }
+}
+console.log(fileiraAuxiliar)
